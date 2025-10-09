2f47f35850857caa489980675a5e9521ff52d5f2
Conduit <153644058+MangoPlayzXD@users.noreply.github.com>
2025-10-08 20:07:36 -0700

    Update and rename template to template.js

---
+++ sneakyCode/template.js
@@ -0,0 +1,5 @@
+let rawcode = 'https://raw.githubusercontent.com/MangoPlayzXD/Userscripts/refs/heads/main/sneakyCode/JSInjector.js'
+
+const script = document.createElement("script");
+script.src = rawcode;
+document.body.appendChild(script);
