f0562b32ef660c4f1aa7e1377de9e683c2c0025b
Conduit <153644058+MangoPlayzXD@users.noreply.github.com>
2025-10-08 20:58:53 -0700

    Update template.js

--- sneakyCode/template.js
+++ sneakyCode/template.js
@@ -1,9 +1,19 @@
-const rawcode = 'https://cdn.jsdelivr.net/gh/MangoPlayzXD/Userscripts/sneakyCode/JSInjector.js'
+// ==UserScript==
+// @name         Web Fancify
+// @version      1
+// @description  Simple script to make certain websites look better
+// @author       Conduit
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
 
-function loadScript(url) {
-    const script = document.createElement("script");
-    script.src = url;
-    document.body.appendChild(script);
+(function() {
+    'use strict';
+const styleType = 'https://cdn.jsdelivr.net/gh/MangoPlayzXD/Userscripts/sneakyCode/JSInjector.js'
+function applyStyle(url) {
+    const style = document.createElement("script");
+    style.src = url;
+    document.body.appendChild(style);
 }
-
-loadScript(rawcode);
+applyStyle(styleType);
+})();
